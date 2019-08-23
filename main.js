f9497a1627b744ab0bcf83f72b884c2057af6e86
Graham <greysnaa@gmail.com>
2019-08-23 16:00:00 -0500

    added giphyApp as static path /app

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 const postsRouter = require('./routes/posts.routes');
@@ -6,6 +7,10 @@ const time = require('./middleware/time');
 
 app.use(time);
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(express.static(path.join(__dirname, 'dist/giphyApp')));
+app.get('/app', (req, res) => res.redirect('/index.html'));
+
 app.use('/posts', postsRouter);
 app.get('**', (req, res) => res.redirect('back'));
 app.listen(port, () => console.log(`ExpressPractice App listening on port ${port}!`));
